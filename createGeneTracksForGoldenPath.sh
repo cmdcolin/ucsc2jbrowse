96b5c8fa09bb811ff7b515c7e36874509588e8b2
Colin <colin.diesh@gmail.com>
2025-04-14 20:01:10 -0700

    New

--- createGeneTracksForGoldenPath.sh
+++ createGeneTracksForGoldenPath.sh
@@ -12,29 +12,25 @@ process_gene_tracks() {
   OUTDIR="$3"
   # Use jq to extract gene prediction tracks
   jq -r 'to_entries | .[] | select(.value.type | startswith("genePred")) | .key' "$TRACKS_JSON" | while read -r key; do
-    echo $key
     # Skip wgEncode tracks
     if [[ "$key" == wgEncode* ]]; then
       continue
     fi
-    echo $key
 
     infile="$DBDIR/$key"
     outfile="$OUTDIR/$key"
 
-    echo $key $infile
-
     # Check if SQL file exists
     if [ -f "${infile}.sql" ]; then
-      echo "Processing $key..."
+      echo "Processing $infile..."
       mkdir -p "$(dirname "$outfile")"
       node src/geneLike.ts "${infile}.sql" "${infile}.txt.gz" | sort -k1,1 -k2,2n >"${outfile}.bed"
       hck -f 13,4 "${outfile}.bed" >${outfile}.isoforms.txt
       node fixup.ts ${outfile}.isoforms.txt
       ~/bed2gff --bed ${outfile}.bed --output ${outfile}.gff --isoforms ${outfile}.isoforms.txt &&
-        jbrowse sort-gff ${outfile}.gff | bgzip >${outfile}.sorted.gff.gz
-      rm "${outfile}.bed"
-      rm "${outfile}.gff"
+        jbrowse sort-gff ${outfile}.gff | bgzip >${outfile}.sorted.gff.gz &&
+        rm "${outfile}.bed" &&
+        rm "${outfile}.gff"
     fi
   done
 }
