7d57ca6a20e839619b001653254ec17725642d48
Colin <colin.diesh@gmail.com>
2025-04-27 20:08:45 -0700

    Only cyto

--- src/addCytobands.ts
+++ src/addCytobands.ts
@@ -1,9 +1,6 @@
-import { readConfig, readJSON, replaceLink, splitOnFirst } from './util.ts'
+import { readConfig } from './util.ts'
 
-import type { TrackDbEntry } from './types.ts'
-
-const config = readConfig(process.argv[2])
-const tracksDb = readJSON(process.argv[3]) as Record<string, TrackDbEntry>
+const config = readConfig(process.argv[2]!)
 
 const cytobands = config.tracks.find(f => f.trackId === 'cytoBandIdeo')
 
@@ -26,36 +23,6 @@ console.log(
             ],
           }
         : {}),
-      tracks: config.tracks.map(t => {
-        const r = tracksDb[t.trackId]
-        if (r) {
-          const { settings, html, longLabel, shortLabel, grp, ...rest } =
-            tracksDb[t.trackId]
-          const s2 = Object.fromEntries(
-            settings
-              .split('\n')
-              .map(r => splitOnFirst(r, ' '))
-              .filter(([key]) => !!key),
-          )
-          return {
-            ...t,
-            metadata: {
-              ...rest,
-              ...s2,
-              html: replaceLink(html),
-            },
-            name: shortLabel,
-            description: longLabel,
-            category: [
-              grp,
-              s2.parent.replace(' on', '').replace(' off', ''),
-            ].filter(f => !!f),
-          }
-        } else {
-          console.error('track not found in trackDb', t.trackId)
-          return t
-        }
-      }),
     },
     null,
     2,
